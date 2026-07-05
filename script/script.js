48d58516342938ce9ba2ea4c8acef76024791c65
NextGenDigitalMarketing <fourtyseven623@gmail.com>
2026-07-05 09:27:27 -0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,35 @@
+// Smooth Scroll
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Navbar Shadow
+
+window.addEventListener("scroll", function () {
+
+const header = document.querySelector("header");
+
+if(window.scrollY > 50){
+
+header.style.background="#0b1120";
+
+header.style.boxShadow="0 8px 20px rgba(0,0,0,.4)";
+
+}
+
+else{
+
+header.style.background="#111827";
+
+header.style.boxShadow="none";
+
+}
+
+});
